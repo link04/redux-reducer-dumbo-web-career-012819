f3b207e09f72f608a429cece22b506c5e716173c
Maximo Bautista <max.bautista97@gmail.com>
2019-04-20 20:13:05 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -12,7 +12,7 @@ export function manageFriends(state, action){
    case 'REMOVE_FRIEND' :
       let stateCopy = {...state };
       stateCopy.friends.filter(friend => 
-        friend.id === 
+        friend.id === action.id
       )
       return stateCopy;
       
