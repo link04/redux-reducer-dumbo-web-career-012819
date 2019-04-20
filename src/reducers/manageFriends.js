29f13863540dd9d031e8e19bb1966c7cd74f174e
Maximo Bautista <max.bautista97@gmail.com>
2019-04-20 20:21:34 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,6 +1,6 @@
 export function manageFriends(state, action){
   switch(action.type){
-    case 'ADD_FRIEND:
+    case 'ADD_FRIEND':
       let stateCopy = {...state };
       stateCopy.friends.push({name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id })
       return stateCopy;
