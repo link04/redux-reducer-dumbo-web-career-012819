0e070760582b813e599cc5e56fe9171effb35fef
Maximo Bautista <max.bautista97@gmail.com>
2019-04-20 20:09:57 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,8 +3,9 @@ export function manageFriends(state, action){
   switch(action.type){
     case 'ADD_FRIEND' :
       let stateCopy = {
-        ...
-      }
+        ...state
+      };
+      
       return Object.assign({}, state, state.friends = [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id }])
     default:state
     
