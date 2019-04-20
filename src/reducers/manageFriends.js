1476a5eec1493489e413096190f1552eec6938cd
Maximo Bautista <max.bautista97@gmail.com>
2019-04-20 20:02:47 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,9 @@
 export function manageFriends(state, action){
+  
+  switch(action.type){
+    case 'ADD_FRIEND' :
+      return Object.assign({}, state, state.friends = [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id }])
+    default:state
+    
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,14 @@
 export function managePresents(state, action){
+  
+  switch(action.type){
+    
+    case 'INCREASE':
+      return Object.assign({}, state, state.numberOfPresents + 1);
+    
+    default:
+      return state;
+     
+  }
+  
 }
+
