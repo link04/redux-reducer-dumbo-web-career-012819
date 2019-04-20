e8ec0c93880676122d9908061c370f65f7154d1e
Maximo Bautista <max.bautista97@gmail.com>
2019-04-20 20:05:22 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -3,6 +3,8 @@ export function managePresents(state, action){
   switch(action.type){
     
     case 'INCREASE':
+      let newState = state;
+      newState.numberOfPresents + 1
       return { ...state, state.numberOfPresents + 1};
     
     default:
